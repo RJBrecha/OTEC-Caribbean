183584d2eeaa24322ee258438c51d81088f612ae
RJBrecha <54171512+RJBrecha@users.noreply.github.com>
2020-08-29 06:44:42 -0400

    Add files via upload

--- RLDC_parameter_scan.py
+++ RLDC_parameter_scan.py
@@ -18,10 +18,10 @@ headerLine   = 1
 nDataRows = 8760
 
 #Read in the Load, Solar and Wind hourly data
-fulldf = pd.read_csv(r"C:\Users\Robert Brecha\Documents\Papers\OTEC and SWAC survey\Residual load data.csv')
+fulldf = pd.read_csv(r"C:\Users\Robert Brecha\Documents\Papers\OTEC and SWAC survey\Residual load data.csv")
 
 #Dispatchable renewable power capacity [MW or GW]
-DispCap = 2
+DispCap = 10
 #Battery storage capacity in MWh or GWh and maximum output power in MW or GW
 #BatteryCap = 10
 #BatteryMaxPower = min(10,BatteryCap)
@@ -29,13 +29,13 @@ DispCap = 2
 
 #set scaling factors such that capacity of both wind and solar are 1MW or 1GW initially; ideally the input data are already scaled so
 # these will then be increased through the loops
-SolarScale = 1
-WindScale = 1
+SolarScale = 0.1
+WindScale = 0.1
 #Extract the load data from the full dataframe; determine the peak load
 LoadData = fulldf.Load
 PeakLoad = max(LoadData)
 #Set the number of hours of battery storage
-BatteryHours = 8
+BatteryHours = 12
 
 #Calculate the residual load for each hour, considering only wind and solar
 #Sort the load curve and the residual load curve from highest to lowest
@@ -50,8 +50,9 @@ Out = np.empty([1,3])
 MaxRLDC = max(LoadData)
 
 #Set the range for scaling factors for scanning the wind and solar pv capacities
-X = np.arange(1,10,1)
-Y = np.arange(1,10,1)
+ScaleMax = 201
+X = np.arange(1,ScaleMax,2)
+Y = np.arange(1,ScaleMax,2)
 
 #Scan through by first setting the wind capacity, then for all solar pv capacities
 #in the respective ranges; at each pair (W, S) calculate the residual load, then for 
@@ -60,13 +61,13 @@ Y = np.arange(1,10,1)
 for W in X:
     for S in Y:
         RL1 = fulldf.Load - S*SolarScale*fulldf.Solar - W*WindScale*fulldf.Wind
-        for i in range(1,100,1):
+        for i in range(1,1001,10):
             BatteryCap = i
             B = []
             RL2 = []
             #BatteryMaxPower = min(10,BatteryCap)
-            BatteryMaxPower = BatteryCap/BatteryHours
-            Batt = BatteryCap/2
+            BatteryMaxPower = max(PeakLoad,BatteryCap/BatteryHours)
+            Batt = BatteryCap*.75
 #scan through all values of residual load; if greater than zero, you need more
 #power.  Either use dispatchable capacity (first choice), or use battery stored
 #energy (second choice).  
@@ -78,19 +79,19 @@ for W in X:
             #            print(x)
                         if x <= BatteryMaxPower and x <= Batt:
                             Batt = Batt - x
-                            B.append(Batt)
+#                            B.append(Batt)
             #                print('Battery=',Batt)
                             x = 0
                         else:
                             x = x - min(BatteryMaxPower, Batt) 
                             Batt = Batt - min(BatteryMaxPower, Batt)
-                            B.append(Batt)
+#                            B.append(Batt)
             #                print('Battery=',Batt)
                     elif x <= DispCap:
                         Diff = DispCap - x
                         x = 0
                         Batt = Batt + min(Diff,BatteryCap-Batt)
-                        B.append(Batt)
+#                        B.append(Batt)
             #       RL2.append(x)    
                 else:
                     if Batt < BatteryCap:
@@ -98,17 +99,17 @@ for W in X:
                         x = x + min(BatteryMaxPower,-x,BatteryCap-Batt)
                         if Batt < BatteryCap:
                            Batt = Batt + min(DispCap,BatteryCap-Batt)
-                    B.append(Batt)
+#                    B.append(Batt)
             #    print(Batt)
+                B.append(Batt)    
                 RL2.append(x) 
-            if max(RL2) <=0.001*max(LoadData):
+            if sum(i for i in RL2 if i > 0) <=0.001*sum(LoadData):
 #                print(BatteryCap)
                 Soln = np.array([W,S,BatteryCap]).reshape(1,3)
                 break
-            BatteryCap = i + BatteryCap
         Soln = np.array([W,S,BatteryCap]).reshape(1,3)
         Out = np.concatenate((Out,Soln),axis = 0)    
-    RLDC2 = sorted(np.array(RL2),reverse=True)
+        RLDC2 = sorted(np.array(RL2),reverse=True)
 
 
     
@@ -135,19 +136,19 @@ ax = plt.axes(projection='3d')
 surf = ax.plot_trisurf(Wind, Solar, Battery, cmap = cm.jet, linewidth = 0)
 fig.colorbar(surf)
 
-ax.set_ylim(10, 0)
+ax.set_ylim(ScaleMax, 0)
 #ax.set_zlim(0,4500)
 ax.xaxis.set_major_locator(MaxNLocator(5))
 ax.yaxis.set_major_locator(MaxNLocator(5))
 ax.zaxis.set_major_locator(MaxNLocator(5))
 #ax.contour3D(Wind, Solar, Out, 50, cmap='binary')
 ax.scatter3D(Wind, Solar, Battery, c=Battery, cmap='Greens');
-ax.set_xlabel('Wind capacity [GW]')
-ax.set_ylabel('Solar pv capacity [GW]')
-ax.set_zlabel('Battery storage [GWh]')
+ax.set_xlabel('Wind capacity [MW]')
+ax.set_ylabel('Solar pv capacity [MW]')
+ax.set_zlabel('Battery storage [MWh]')
 
 fig.tight_layout()
 plt.show()
-fig.savefig('DispCap_1a.png')
+fig.savefig(r"C:\Users\Robert Brecha\Documents\Papers\OTEC and SWAC survey\DispCap_10d_new.png")
 
-np.savetxt(r"C:\Users\Robert Brecha\Documents\Energy System Modeling\Australian energy system\DispCap1a.txt",Out)
+np.savetxt(r"C:\Users\Robert Brecha\Documents\Papers\OTEC and SWAC survey\DispCap_10d_new.txt",Out)
